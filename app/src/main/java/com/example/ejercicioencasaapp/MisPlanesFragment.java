6832fc65263433769a02e27c3f03883d0d2ae2a9
= <=>
2020-09-20 14:31:42 -0500

    Creada la activity principal, se muestra un Tab Navigator con dos fragments, las opciones del tab navigator son Ejercicios y Mis Planes, se le da un diseño tipo slider al Tab Navigator

--- app/src/main/java/com/example/ejercicioencasaapp/MisPlanesFragment.java
+++ app/src/main/java/com/example/ejercicioencasaapp/MisPlanesFragment.java
@@ -28,7 +28,6 @@ public class MisPlanesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_mis_planes, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_mis_planes, container, false);
     }
 }
