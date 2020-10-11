0473e006f57641efcdc99c6c8e95dcc4e8c96ea0
= <=>
2020-10-10 23:18:08 -0500

    cree el reproductor de la rutina, empiza una cuenta regresiva, y un boton que pausa y reanuda la cuenta regresiva, falta añadir que vayan pasando los ejercicios de la rutina a mediada que las cuentas regresivas van terminando

--- app/src/main/java/com/example/ejercicioencasaapp/views/ReproductorRutinaActivity.java
+++ app/src/main/java/com/example/ejercicioencasaapp/views/ReproductorRutinaActivity.java
@@ -1,9 +1,14 @@
 package com.example.ejercicioencasaapp.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.ejercicioencasaapp.R;
@@ -11,7 +16,11 @@ import com.example.ejercicioencasaapp.R;
 public class ReproductorRutinaActivity extends AppCompatActivity {
     public static final String EXTRA_REPRODUCTOR = "rutinaNombre";
     private String nombreRutina;
-    private TextView tvNombreRutina;
+    private TextView tvNombreRutina, tvCurrentEjercicio;
+    private Button btnPlay;
+    private int seconds = 30;
+    public int count = 0;
+    private boolean running, wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +33,72 @@ public class ReproductorRutinaActivity extends AppCompatActivity {
         tvNombreRutina = (TextView)findViewById(R.id.tvReproductor);
         tvNombreRutina.setText(String.valueOf(nombreRutina));
 
+        tvCurrentEjercicio = (TextView)findViewById(R.id.tvCurrentEjercicio);
+        tvCurrentEjercicio.setText(String.valueOf(count));
+        running = true;
+        btnPlay = (Button)findViewById(R.id.btnPlay);
+
+        if(savedInstanceState != null){
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+        }
+        runTimer();
+    }
+    //al clikear el boton conmuta el estado running y cambia el texto del boton
+    public void onClickPause(View view){
+        running = !running;
+        if(running == true){
+            btnPlay.setText("pause");
+        }else{
+            btnPlay.setText("play");
+        }
+    }
+
+    @Override
+    //Al volver a la ventana de la aplicacion el reproductor sigue pausado hasta que se de click al boton
+    protected void onStart() {
+        super.onStart();
+        if(wasRunning){
+            running = true;
+            btnPlay.setText("pause");
+        }
+    }
+
+    @Override
+    //Al cambiar de ventana en el telefono el reproductor se pausa automaticamente
+    protected void onStop() {
+        super.onStop();
+        wasRunning = false;
+        running = false;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+    }
+
+    private void runTimer(){
+        final TextView timeView = (TextView)findViewById(R.id.tvTimer);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                int sec = seconds%60;
+                String time = String.format("%02d ",sec);
+                timeView.setText(time);
+                if(running == true){
+                    seconds--;
+                }
+                if(seconds == 0){
+                    count++;
+                    tvCurrentEjercicio.setText(String.valueOf(count));
+                    seconds = 30;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
     }
 }
